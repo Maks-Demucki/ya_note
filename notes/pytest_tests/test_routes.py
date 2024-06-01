1e79710e9619d05148549a6c9eca416f1fe939dc
Maks Demucki <ya.maksim777d@yandex.ru>
2024-06-01 20:06:01 +0600

    Тест доступности страниц для автора заметки

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -28,3 +28,13 @@ def test_pages_availability_for_auth_user(not_author_client, name):
     url = reverse(name)
     responce = not_author_client.get(url)
     assert responce.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
