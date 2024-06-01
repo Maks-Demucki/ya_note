e51079bda932959ad1c27a684974f8be0f48e097
Maks Demucki <ya.maksim777d@yandex.ru>
2024-06-01 19:38:17 +0600

    проверка доступности для вскх пользователей регистрации

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,3 +1,4 @@
+import pytest
 from http import HTTPStatus
 
 from django.urls import reverse
@@ -7,3 +8,14 @@ def test_home_availability_for_anonymous_user(client):
     url = reverse('notes:home')
     responce = client.get(url)
     assert responce.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    responce = client.get(url)
+    assert responce.status_code == HTTPStatus.OK
+
