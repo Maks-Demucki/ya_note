9cbb057c5b30be51a79f5db4764f3a2500b7a484
Maks Demucki <ya.maksim777d@yandex.ru>
2024-06-02 20:46:25 +0600

    Тест создания заметки зарегистрированным и анонимным пользователем

---
+++ notes/pytest_tests/test_logic.py
@@ -0,0 +1,29 @@
+import pytest
+
+from pytest_django.asserts import assertRedirects
+
+from django.urls import reverse
+
+from notes.models import Note
+
+
+def test_user_can_create_note(author_client, author, form_data):
+    url = reverse('notes:add')
+    responce = author_client.post(url, data=form_data)
+    assertRedirects(responce, reverse('notes:success'))
+    assert Note.objects.count() == 1
+    new_note = Note.objects.get()
+    assert new_note.title == form_data['title']
+    assert new_note.text == form_data['text']
+    assert new_note.slug == form_data['slug']
+    assert new_note.author == author
+
+
+@pytest.mark.django_db
+def test_anonymous_user_cant_create_note(client, form_data):
+    url = reverse('notes:add')
+    responce = client.post(url, data=form_data)
+    login_url = reverse('users:login')
+    expected_url = f'{login_url}?next={url}'
+    assertRedirects(responce, expected_url)
+    assert Note.objects.count() == 0
