4cc21eb1bbd4ba32c0c4d91db1ac4e814a60c175
Maks Demucki <ya.maksim777d@yandex.ru>
2024-06-03 19:44:47 +0600

    Тест проверки удаления заметок

--- notes/pytest_tests/test_logic.py
+++ notes/pytest_tests/test_logic.py
@@ -70,4 +70,18 @@ def test_other_user_cant_edit_note(not_author_client, form_data, note):
     note_from_db = Note.objects.get(id=note.id)
     assert note.title == note_from_db.title
     assert note.text == note_from_db.text
-    assert note.slug == note_from_db.slug 
+    assert note.slug == note_from_db.slug
+
+
+def test_author_can_delete_note(author_client, slug_for_args):
+    url = reverse('notes:delete', args=slug_for_args)
+    response = author_client.post(url)
+    assertRedirects(response, reverse('notes:success'))
+    assert Note.objects.count() == 0
+
+
+def test_other_user_cant_delete_note(not_author_client, slug_for_args):
+    url = reverse('notes:delete', args=slug_for_args)
+    response = not_author_client.post(url)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Note.objects.count() == 1 
